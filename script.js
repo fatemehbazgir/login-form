36f0f9b507efb6a1a9f5438f560843ff762a8dd2
Fatemeh Bazgir <fatemehbazgir2018@gmail.com>
2023-12-08 22:36:08 +0330

    add html and css and js code

---
+++ script.js
@@ -0,0 +1,7 @@
+const wrapper = document.querySelector(".wrapper");
+const registerLink = document.querySelector(".register-link");
+const loginLink = document.querySelector(".login-link");
+
+registerLink.onclick = () => {
+  wrapper.classList.add("active");
+};
